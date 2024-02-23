17403daadf8ebfa96028d6c713f37217c2c7061d
Clinton Nogueira Silva <126696407+clintonsilvas@users.noreply.github.com>
2024-02-23 17:35:57 +0000

    mailform

--- enviar_email.php
+++ enviar_email.php
@@ -1,10 +1,10 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Recupera os valores do formulário
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $telefone = $_POST['telefone'];
-    $mensagem = $_POST['mensagem'];
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $telefone = addslashes($_POST['telefone']);
+    $mensagem = addslashes($_POST['mensagem']);
 
     // Destinatário do e-mail
     $destinatario = "serranegramc@redenets.com.br";
@@ -20,7 +20,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Cabeçalhos do e-mail
     $headers = "De: $nome <$email>\r\n";
-    $headers .= "Responder para: $email\r\n";
+    $headers .= "Responder para: $email\r\n".
+    "X-Mailer: PHP/" . phpversion();
+
+
 
     // Envia o e-mail
     if (mail($destinatario, $assunto, $conteudo, $headers)) {
